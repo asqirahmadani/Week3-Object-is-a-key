14e2e78ebad9a83be6a69285e6ee54ff8c05200e
asqirahmadani <rahmadaniasqi@gmail.com>
2025-03-25 15:34:56 +0700

    Week3 Ujian

---
+++ quiz/ujian/soal2.js
@@ -0,0 +1,41 @@
+/*
+Diberikan function naikAngkot(listPenumpang) yang akan menerima satu parameter berupa array dua dimensi. Function akan me-return array of object.
+
+Diberikan sebuah rute, dari A - F. Penumpang diwajibkan membayar Rp2000 setiap melewati satu rute.
+
+Contoh: input: [['Dimitri', 'B', 'F']] output: [{ penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 }]
+*/
+
+function naikAngkot(arrPenumpang) {
+    rute = ['A', 'B', 'C', 'D', 'E', 'F'];
+    //your code here
+
+    let result = [];
+
+    if (arrPenumpang.length == 0) {
+        return result;
+    }
+    let harga = 2000;
+
+    for (let i = 0; i < arrPenumpang.length; i++) {
+        let asal = rute.indexOf(arrPenumpang[i][1]);
+        let tujuan = rute.indexOf(arrPenumpang[i][2]);
+        let bayar = (Math.abs(asal - tujuan)) * harga;
+
+        let data = {
+            penumpang: arrPenumpang[i][0],
+            naikDari: arrPenumpang[i][1],
+            tujuan: arrPenumpang[i][2],
+            bayar: bayar
+        }
+        result.push(data);
+    }
+    return result;
+}
+
+//TEST CASE
+console.log(naikAngkot([['Dimitri', 'B', 'F'], ['Icha', 'A', 'B']]));
+// [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+//   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+
+console.log(naikAngkot([])); //[]
